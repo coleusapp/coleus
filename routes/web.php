71c0447a881c59ee9ea0fa0db8efb17c4d383a3f
Amirmasoud <amirmasood33@gmail.com>
2023-09-14 17:39:52 -0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    abort(418);
-});
+Route::get('/desktop', function () {
+    return [
+        'authenticated' => \Illuminate\Support\Facades\Auth::check()
+    ];
+})->middleware('auth');
