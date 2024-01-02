8dc67c17df6b8ab98674adee80a738dd96a9bbfe
Amirmasoud <amirmasood33@gmail.com>
2024-01-01 22:55:23 -0700

    Add onlyme middleware for now

---
+++ app/Http/Middleware/OnlyMe.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class OnlyMe
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            abort_if(auth()->user()?->email !== 'amirmasood33@gmail.com', 403, ':)');
+        }
+
+        return $next($request);
+    }
+}
