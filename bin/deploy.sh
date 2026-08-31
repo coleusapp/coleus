be4bb1975d3b5d0c0ce61eb474046cf1300dfac5
Amirmasoud <amirmasood33@gmail.com>
2026-08-30 19:19:10 -0600

    Update coleus packages to v1.0.48

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -14,13 +14,13 @@ fi
 
 composer update 'coleus/*' --with-all-dependencies --no-interaction
 
-git add composer.json composer.lock public/*
+git add .
 git commit -m "Update coleus packages to $VERSION"
 git push origin main
 
-if [[ -n "$DOCKERHUB_USERNAME" && -n "$DOCKERHUB_TOKEN" ]]; then
-    echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-fi
+#if [[ -n "$DOCKERHUB_USERNAME" && -n "$DOCKERHUB_TOKEN" ]]; then
+#    echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+#fi
 
 docker buildx build --platform linux/amd64,linux/arm64 -t "coleus/coleus:$VERSION" -t "coleus/coleus:latest" --push .
 
